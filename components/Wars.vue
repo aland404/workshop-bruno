acb71272726e508ba922d8352ceac2da359d1efc
Alan Duchêne <alan.duchene@zenika.com>
2024-06-23 14:39:57 +0200

    components(wars): add message and confettis for victory

--- components/Wars.vue
+++ components/Wars.vue
@@ -1,16 +1,40 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import BattleInfo from "./BattleInfo.vue";
+import BattleInfo from "./BattleInfo.vue"
+import JSConfetti from 'js-confetti'
 
-const wars = ref([])
+const jsConfetti = new JSConfetti()
+
+const warsInfo = ref([])
 const fetchHasFailed = ref(false)
 const loading = ref(false)
+const isRebellionWinning = ref(false)
 
 async function fetchWars() {
   loading.value = true
   try {
     const response = await fetch("http://localhost:3000/star-wars/wars")
-    wars.value = await response.json()
+    warsInfo.value = await response.json()
+    if(warsInfo.value.summary.winner === 'rebellion') {
+      isRebellionWinning.value = true
+      jsConfetti.addConfetti({
+        emojis: ['🌟', '✨', '⚔️', '💫'],
+        emojiSize: 50,
+      });
+      jsConfetti.addConfetti({
+        confettiColors: [
+          '#0057e7', // Bleu sabre laser
+          '#d32f2f', // Rouge sabre laser
+          '#f4f4f4', // Blanc (Stormtroopers)
+          '#000000', // Noir (Dark Vador, l'espace)
+          '#ffe81f', // Jaune (C3PO, certaines planètes)
+          '#7e7e7e', // Gris (vaisseaux)
+        ],
+        confettiNumber: 250,
+      })
+    } else {
+      isRebellionWinning.value = false
+    }
   } catch(error) {
     fetchHasFailed.value = true
   } finally {
@@ -30,7 +54,7 @@ onMounted(() => {
 
     <div v-if="loading" class="loader-container"><span class="loader"></span></div>
     <div v-else-if="!fetchHasFailed">
-      <div v-for="war in wars" :key="`war-${war.name}`">
+      <div v-for="war in warsInfo.wars" :key="`war-${war.name}`">
         <b>Guerre: "{{war.name}}"</b>
         <ul v-for="battle in war.battles" :key="`battle-${battle.name}`">
           <BattleInfo :war-slug="war.slug" :battle-slug="battle.slug" />
@@ -40,6 +64,11 @@ onMounted(() => {
     <div v-else>Le serveur ne semble pas être démarré en local</div>
     <br />
 
+    <div v-if="isRebellionWinning" id="rebellion-wins-message">
+      <div>👏 BRAVO 👏</div>
+      <div>VOUS AVEZ PERMIS A LA REBELLION DE GAGNER</div>
+      <div>LA GUERRE CONTRE l'EMPIRE!</div>
+    </div>
     <div class="button-container">
       <input type="button" class="button" @click="fetchWars" value="Rafraichir les données" :disabled="loading" />
     </div>
@@ -56,6 +85,9 @@ onMounted(() => {
   border: 1px solid white;
   padding: 0.5rem;
   border-radius: 5px;
+}
+#rebellion-wins-message {
+  text-align: center;
 }
 .button-container {
   display: flex;
